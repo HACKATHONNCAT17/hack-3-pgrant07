b28de632d1a0c0def3e4d9f204034afba6e4edd4
pgrant07 <p.grant012@gmail.com>
2017-09-30 11:32:51 -0400

    updated layout pickdate

--- AggieTicketbox/app/src/main/java/com/example/pgran/aggieticketbox/DatePickActivity.java
+++ AggieTicketbox/app/src/main/java/com/example/pgran/aggieticketbox/DatePickActivity.java
@@ -1,13 +1,23 @@
 package com.example.pgran.aggieticketbox;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 
 public class DatePickActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    final Calendar c= Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_pick);
+        datePicker=(DatePicker) findViewById(R.id.datePicker);
+        datePicker.updateDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
     }
 }
---
+++ AggieTicketbox/app/src/main/java/com/example/pgran/aggieticketbox/Ticket.java
@@ -0,0 +1,9 @@
+package com.example.pgran.aggieticketbox;
+
+/**
+ * Created by pgran on 9/30/2017.
+ */
+
+public class Ticket {
+
+}
